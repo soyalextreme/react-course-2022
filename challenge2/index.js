2ebc42f813d88e955e1624804cb08ea96b442309
Alejandro AS <soyalextreme@gmail.com>
2022-01-22 20:51:51 -0600

    Commit Changes: [add] Styles to the components [add] Practice with the class names

--- challenge2/index.js
+++ challenge2/index.js
@@ -1,33 +1,63 @@
 // import React from "react";
 // import ReactDOM from "react-dom";
 
+const Nav = () => {
+    const navElements = ["Pricing", "Contact", "About Us"]
+
+    return (
+        <nav className="Nav">
+            <ul className="Nav-List">
+                {navElements.map(element => <li className="Nav-List__element">{element}</li>)}
+            </ul>
+        </nav>
+    );
+}
+
+
+const Footer = () => (
+    <footer className="Footer">
+        <small className="Footer__content">
+            2022 Alejandro Andrade Dev, All rights reserved.
+        </small>
+    </footer>
+);
+
 
 function Header() {
     return (
-        <header>
-            <h1>Reasons I want to learn React!</h1>
+        <header className="Header">
+            <div className="Header-Logo">
+                <img src="./static/react-logo.png" alt="react-logo" />
+            </div>
+            <Nav />
         </header>
     );
 }
 
 
 const ListOfReasons = () => (
-    <ol>
-        <li>I want to become better programmer</li>
-        <li>I want to build awesome websites</li>
-        <li>I want to have good bases for React Native</li>
-        <li>I want to have a job and have good earnings</li>
+    <ol className="MainContent-List">
+        <li className="MainContent-List__element">I want to become better programmer</li>
+        <li className="MainContent-List__element">I want to build awesome websites</li>
+        <li className="MainContent-List__element">I want to have good bases for React Native</li>
+        <li className="MainContent-List__element">I want to have a job and have good earnings</li>
     </ol>
 );
 
+
+const MainContent = () => (
+    <section className="MainContent">
+        <h1>Reasons I want to learn React!</h1>
+        <ListOfReasons />
+    </section>
+);
 
 function Root() {
     return (
         <div>
             <Header />
-            <section>
-                <ListOfReasons />
-            </section>
+            <MainContent />
+            <Footer />
         </div>
     );
 }
